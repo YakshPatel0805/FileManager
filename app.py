3e52e9fd9b38e73a2ce27f48a969c6e72cc551a8
Yaksh Patel <yakshpatel6464@gmail.com>
2026-01-19 17:09:59 +0530

    Enhance email setup and user feedback in app.py
    Added email configuration and improved user feedback with flash messages.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, abort, send_file
+from flask import Flask, render_template, request, flash, redirect, url_for, session, abort, send_file
 from pymongo import MongoClient
 import bcrypt, re
 from bson import ObjectId
@@ -14,9 +14,14 @@ app.secret_key = "super_secret_key"
 # email configration
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True# Max file size = 16 MB
+app.config["MAIL_USE_TLS"] = True
 app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024  # 16MB
 MAX_FILE_SIZE = 16 * 1024 * 1024
+app.config["MAIL_USERNAME"] = "esalpha337@gmail.com"
+app.config["MAIL_PASSWORD"] = "mydi jyoz ehkl tctl"
+app.config["MAIL_DEFAULT_SENDER"] = "esalpha337@gmail.com"
+app.config["MAIL_DEBUG"] = True
+
 
 mail = Mail(app)
 serializer = URLSafeTimedSerializer(app.secret_key)
@@ -26,7 +31,7 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client["user_db"]
 users_collection = db["users"]
 files_collection = db["files"]
-# users_collection.create_index('email', unique=True)
+users_collection.create_index("email", unique=True)
 
 
 # Email verification link
@@ -89,21 +94,24 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        email = request.form.get("email")
-        password = request.form.get("password").encode('utf-8')
+        email = request.form.get("email").lower()
+        password = request.form.get("password").encode("utf-8")
 
-        user = users_collection.find_one({"email": email.lower()})
-        if user and bcrypt.checkpw(password, user["password"]):
+        user = users_collection.find_one({"email": email})
 
-            # # Email verification before login
-            # if not user.get('is_verified'):
-            #     return render_template('login.html', error='Please verify email first...')
+        if not user or not bcrypt.checkpw(password, user["password"]):
+            flash("Invalid email or password", "danger")
+            return redirect(url_for("login"))
 
-            session["email"] = email.lower()
-            session["is_admin"] = True if email == "admin@gmail.com" else False
-            return redirect(url_for("index"))
+        if not user.get("is_verified"):
+            flash("Please verify your email first", "info")
+            return redirect(url_for("login"))
 
-        return render_template("login.html", error="Invalid email or password")
+        session["email"] = email
+        session["is_admin"] = email == "esalpha337@gmail.com"
+
+        flash("Logged in successfully", "success")
+        return redirect(url_for("index"))
 
     return render_template("login.html")
 
@@ -131,15 +139,21 @@ def signup():
 
         strong, error_msg = isStrong(password)
         if not strong:
-            return render_template("signup.html", error=error_msg)
+            flash(error_msg, "danger")
+            return redirect(url_for("signup"))
 
         if password != re_password:
-            return render_template("signup.html", error="Passwords do not match")
+            flash("Passwords do not match", "danger")
+            return redirect(url_for("signup"))
 
         if users_collection.find_one({"email": email.lower()}):
-            return render_template("signup.html", error="User already exists")
+            flash("User already exists", "danger")
+            return redirect(url_for("signup"))
 
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        hashed_password = bcrypt.hashpw(
+            password.encode("utf-8"),
+            bcrypt.gensalt()
+        )
 
         users_collection.insert_one({
             "email": email.lower(),
@@ -148,10 +162,13 @@ def signup():
             "created_at": datetime.now()
         })
 
-        # # Email verification
-        # send_verification_email(email.lower())
+        send_verification_email(email.lower())
 
-        return render_template("login.html", success="Verification email sent. Please check your inbox.")
+        flash(
+            "Verification email sent. Please check your inbox.",
+            "success"
+        )
+        return redirect(url_for("login"))
 
     return render_template("signup.html")
 
@@ -162,11 +179,14 @@ def forgot_password():
     if request.method == "POST":
         email = request.form.get("email").lower()
 
-        user = users_collection.find_one({"email": email})
-        if user:
+        if users_collection.find_one({"email": email}):
             send_password_reset_email(email)
 
-        return render_template("forget.html", success="If this email exists, a reset link has been sent.")
+        flash(
+            "If this email exists, a reset link has been sent.",
+            "info"
+        )
+        return redirect(url_for("login"))
 
     return render_template("forget.html")
 
@@ -178,7 +198,7 @@ def reset_password(token):
         email = serializer.loads(
             token,
             salt="password-reset",
-            max_age=1800  # 30 minutes
+            max_age=1800
         )
     except SignatureExpired:
         return "Reset link expired", 400
@@ -207,7 +227,8 @@ def reset_password(token):
             return render_template("reset.html", error=error_msg)
 
         if password != re_password:
-            return render_template("reset_password.html", error="Passwords do not match")
+            flash("Password does not match", "danger")
+            return render_template("reset_password.html")
 
         hashed_password = bcrypt.hashpw(
             password.encode("utf-8"),
@@ -219,7 +240,8 @@ def reset_password(token):
             {"$set": {"password": hashed_password}}
         )
 
-        return render_template("login.html", success="Password reset successful. Please log in.")
+        flash("Password reset successful. Please log in.", "success")
+        return redirect(url_for("login"))
 
     return render_template("reset_password.html")
 
@@ -238,22 +260,27 @@ def verify_email(token):
         email = serializer.loads(
             token,
             salt="email-verify",
-            max_age=3600  # 1 hour
+            max_age=3600
         )
     except SignatureExpired:
-        return "Verification link expired", 400
+        flash("Verification link expired", "danger")
+        return redirect(url_for("login"))
     except BadSignature:
-        return "Invalid verification link", 400
+        flash("Invalid verification link", "danger")
+        return redirect(url_for("login"))
 
     users_collection.update_one(
         {"email": email},
-        {"$set": {"is_verified": True, "verified_at": datetime.now()}})
+        {"$set": {"is_verified": True, "verified_at": datetime.now()}}
+    )
 
-    return render_template(
-        "login.html",
-        success="Email verified successfully. You can now log in.")
+    session['email'] = email
+
+    flash("Email verified successfully. You can now log in.", "success")
+    return redirect(url_for("login"))
 
 
+# upload files
 @app.route("/upload", methods=["GET", "POST"])
 def upload_file():
     if not session.get("email"):
@@ -265,9 +292,9 @@ def upload_file():
         if not file or file.filename == "":
             return render_template("upload.html", error="No file selected")
 
-        file.seek(0, 2)   # move cursor to end
+        file.seek(0, 2)
         file_size = file.tell()
-        file.seek(0)      # reset cursor
+        file.seek(0)
 
         if file_size > MAX_FILE_SIZE:
             return render_template(
@@ -282,8 +309,8 @@ def upload_file():
             "uploaded_by": session["email"],
             "upload_date": datetime.now()
         })
-
-        return render_template("upload.html", success="File uploaded successfully")
+        flash("File Upload Successfully", 'success')
+        return render_template("upload.html")
 
     return render_template("upload.html")
 
@@ -336,9 +363,9 @@ def admin_dashboard():
 
 @app.errorhandler(413)
 def file_too_large(error):
+    flash("File too large. Maximum allowed size is 16 MB.",'danger')
     return render_template(
         "upload.html",
-        error="File too large. Maximum allowed size is 16 MB."
     ), 413
 
 
