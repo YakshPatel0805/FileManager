5cc483f1862c7ae54fcb79b3244422bae95150b4
Yaksh Patel <yakshpatel6464@gmail.com>
2026-02-06 17:22:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024  # 16MB
 MAX_FILE_SIZE = 16 * 1024 * 1024
-app.config["MAIL_USERNAME"] = "esalpha337@gmail.com"
+app.config["MAIL_USERNAME"] = "your_gmail@gmail.com"
 app.config["MAIL_PASSWORD"] = "App_Password"  # 16 digit
-app.config["MAIL_DEFAULT_SENDER"] = "esalpha337@gmail.com"
+app.config["MAIL_DEFAULT_SENDER"] = "your_gmail@gmail.com"
 app.config["MAIL_DEBUG"] = True
 
 
@@ -404,4 +404,5 @@ def file_too_large(error):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
